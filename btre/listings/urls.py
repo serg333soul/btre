bde57937d273017c78c675718fbe1d9f58d8a4b2
Sergey <serg333soul@gmail.com>
2019-12-26 16:44:45 +0200

    end 1 part of lesson 29

---
+++ btre/listings/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+# Create your tests here.
+
+urlpatterns = [
+    path('', views.index, name='listings'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search', views.search, name='search'),
+]
