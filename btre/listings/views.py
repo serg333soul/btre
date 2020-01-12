c55ac934a785f3a86b4279e631d9c405fda9f0d7
Sergey Ruban <serg333soul@gmail.com>
2020-01-12 20:54:27 +0200

    lesson 45 09:00

--- btre/listings/views.py
+++ btre/listings/views.py
@@ -28,11 +28,18 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)    
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
 
     context = {
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'listings': queryset_list
     }
 
     return render(request, 'listings/search.html', context)       
