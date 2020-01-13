7d37574598c32d596701d014fcb47f3e3464b8fe
Sergey <serg333soul@gmail.com>
2020-01-13 08:54:37 +0200

    end lesson 45

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
+        'NAME': 'django1',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
