a45fa546f6ba569017537b29811e2f4e631fefc7
Sergey Ruban <serg333soul@gmail.com>
2020-01-14 22:29:20 +0200

    end lesson 48

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        #Register user
+        return
+    else:    
+        return render(request, 'accounts/register.html')
 
 def login(request):
+    if request.method == 'POST':
+        #Login user
+        return
+    else:    
+        return render(request, 'accounts/login.html')
     return render(request, 'accounts/login.html')
 
 def logout(request):
