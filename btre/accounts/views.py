0a20ada46053432adb5bd8b4e9ab1288a9d05c63
Sergey Ruban <serg333soul@gmail.com>
2020-01-16 22:38:58 +0200

    lesson 50 07:00

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That  username is taken')
+                return redirect('register')
+            else:    
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # Good
+                    return
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')    
     else:    
         return render(request, 'accounts/register.html')
 
