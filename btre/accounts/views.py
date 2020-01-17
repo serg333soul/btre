cd429cc036ef09c8dc0c3733200d12fed9432187
Sergey <serg333soul@gmail.com>
2020-01-17 08:47:33 +0200

    end lesson 50

--- btre/accounts/views.py
+++ btre/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -24,7 +24,15 @@ def register(request):
                     return redirect('register')
                 else:
                     # Good
-                    return
+                    user = User.objects.create_user(username=username, password=password, 
+                    email=email, first_name=first_name, last_name=last_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are now registerd and can log in')
+                    return redirect('login')
         else:
             messages.error(request, 'Password do not match')
             return redirect('register')    
