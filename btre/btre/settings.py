fa692cbed8304cae8be4dbcb4d40cf7c9ecbf90b
Sergey <serg333soul@gmail.com>
2020-01-20 12:13:34 +0200

    end lesson 54

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
+        'NAME': 'django1',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
